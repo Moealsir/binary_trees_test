3b5fe4746a1adbdd5a0629ab13fb002c806b5bf0
Moealsir <mohamedwdalsir@gmail.com>
2024-02-27 20:21:56 +0300

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,1 +1,19 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_balance - a Function that ...
+ * @tree: Description of tree.
+ * Return: Description of the return value.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	if (!tree)
+		return (0);
+
+	height_l = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
+	height_r = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	return (height_l - height_r);
+}
+
---
+++ height.c
@@ -0,0 +1,6 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - a Function that ...
+ * @tree: Description of tree.
+ * Return: Description of the return value.
+ */

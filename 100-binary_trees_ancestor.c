438779e76dd4c6d197ff42d7e6e4452caccc6dfc
Moealsir <mohamedwdalsir@gmail.com>
2024-02-28 11:23:23 +0300

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -5,29 +5,26 @@
  * @second: Description of second.
  * Return: Description of the return value.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-	const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
-	const binary_tree_t temp;
+    if (first == NULL || second == NULL)
+        return NULL;
 
-	if (!first || !second)
-		return (NULL);
+    if (first == second)
+        return (binary_tree_t *)first;
 
-	if (first == second)
-		return ((binary_tree_t *) first);
+    while (first != NULL)
+    {
+        const binary_tree_t *temp = second;
 
-	while (first)
-	{
-		temp = second;
-		while (temp)
-		{
-			if (first == temp)
-				return ((binary_tree_t *)first)
-					temp = temp->parent;
-		}
-		first = first->parent;
-	}
-	return (NULL);
+        while (temp != NULL)
+        {
+            if (first == temp)
+                return (binary_tree_t *)first;
+            temp = temp->parent;
+        }
+        first = first->parent;
+    }
 
+    return NULL;
 }
-
